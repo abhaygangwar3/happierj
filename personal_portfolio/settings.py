1b94163af82a05f354ae6eeec0ff872d9976e6b4
rupal <gangwarabhay@outlook.com>
2020-05-17 11:07:27 +0000

    Change in settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't!bi^kal==qf2b=0#n_$^l7qo!4h40(21ce^)1ky^4ayc=$bxw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['happierj.pythonanywhere.com']
 
 
 # Application definition
